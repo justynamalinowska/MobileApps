11064d6f7791267bc70de37ba8961096cdec906f
Justyna Malinowska <justynamalinowska758@gmail.com>
2025-03-12 15:03:12 +0100

    Add rocket pic

--- app/src/main/java/pl/wsei/pam/lab02/Lab02Activity.kt
+++ app/src/main/java/pl/wsei/pam/lab02/Lab02Activity.kt
@@ -1,33 +1,21 @@
-package pl.wsei.pam.lab01
+package pl.wsei.pam.lab02
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pl.wsei.pam.lab01.R
 
-class MainActivity : AppCompatActivity() {
+class Lab02Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_lab02)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-
-    fun onClickMainBtnRunLab01(v: View){
-        val intent = Intent(this, Lab01Activity::class.java)
-        startActivity(intent)
-    }
-
-    fun onClickMainBtnRunLab02(v: View){
-        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
-    }
-
 }
