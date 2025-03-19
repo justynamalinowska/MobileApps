1d5617e3b19bf0e41a0d4549badad582104b1d8b
Justyna Malinowska <justynamalinowska758@gmail.com>
2025-03-19 15:16:21 +0100

    Add tile

--- app/src/main/java/pl/wsei/pam/lab02/Lab02Activity.kt
+++ app/src/main/java/pl/wsei/pam/lab02/Lab02Activity.kt
@@ -1,5 +1,6 @@
 package pl.wsei.pam.lab02
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pl.wsei.pam.lab01.R
+import pl.wsei.pam.lab03.Lab03Activity
 
 class Lab02Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,9 +24,25 @@ class Lab02Activity : AppCompatActivity() {
         }
     }
 
-    fun OnBoardSizeButtonClicked(v: View){
+    fun onBoardSizeButtonClicked(v: View) {
         val buttonView: Button = v as Button
-        Toast.makeText(this, buttonView.text, Toast.LENGTH_SHORT).show()
-    }
+        val intent = Intent(this, Lab03Activity::class.java)
 
+        val buttonText = buttonView.text.toString()
+
+        val parts = buttonText.split(" x ")
+
+        if (parts.size == 2) {
+            val rows = parts[0].toInt()
+            val columns = parts[1].toInt()
+
+            intent.putExtra("rows", rows)
+            intent.putExtra("columns", columns)
+        } else {
+            intent.putExtra("rows", 3)
+            intent.putExtra("columns", 3)
+        }
+
+        startActivity(intent)
+    }
 }
--- app/src/main/java/pl/wsei/pam/lab03/Lab03Activity.kt
+++ app/src/main/java/pl/wsei/pam/lab03/Lab03Activity.kt
@@ -1,10 +1,14 @@
 package pl.wsei.pam.lab03
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.gridlayout.widget.GridLayout
 import pl.wsei.pam.lab01.R
 
 class Lab03Activity : AppCompatActivity() {
@@ -17,5 +21,24 @@ class Lab03Activity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val mBoard = findViewById<GridLayout>(R.id.gridLayout)
+
+        mBoard.columnCount = intent.getIntExtra("columns", 3)
+        mBoard.rowCount = intent.getIntExtra("rows", 3)
+
+        val btn = ImageButton(this).also {
+            it.tag = "${mBoard.rowCount}x${mBoard.columnCount}"
+            val layoutParams = GridLayout.LayoutParams()
+            it.setImageResource(R.drawable.baseline_rocket_launch_24)
+            layoutParams.width = 0
+            layoutParams.height = 0
+            layoutParams.setGravity(Gravity.CENTER)
+            layoutParams.columnSpec = GridLayout.spec(mBoard.columnCount, 1, 1f)
+            layoutParams.rowSpec = GridLayout.spec(mBoard.rowCount, 1, 1f)
+            it.layoutParams = layoutParams
+            mBoard.addView(it)
+        }
+
+
     }
 }
